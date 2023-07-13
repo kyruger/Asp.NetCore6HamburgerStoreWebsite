e1ac5f29f69565ff4603a2d37260b339ac29c04f
Fatih Tarakcı <fatih_trkci96@hotmail.com>
2023-07-13 22:05:26 +0300

    com

---
+++ Hamburger_Application/Areas/User/Controllers/AccountController.cs
@@ -0,0 +1,154 @@
+﻿using AutoMapper;
+using Hamburger_Application.Areas.User.Models.AppUserVMs;
+using Hamburger_Application.Entities.Concrete;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace Hamburger_Application.Areas.User.Controllers
+{
+	[Area("User")]
+	public class AccountController : Controller
+	{
+		private readonly UserManager<AppUser> userManager;
+		private readonly SignInManager<AppUser> signInManager;
+		private readonly IMapper mapper;
+
+		public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IMapper mapper)
+		{
+			this.userManager = userManager;
+			this.signInManager = signInManager;
+			this.mapper = mapper;
+		}
+
+		public IActionResult SignUp()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SignUpAsync(AppUserCreateVM appUserCreateVM)
+		{
+			if (ModelState.IsValid)
+			{
+				Random random = new();
+				int randomCode = random.Next(100_000, 1_000_000);
+
+				AppUser appUser = new();
+				appUser = mapper.Map<AppUser>(appUserCreateVM);
+
+				appUser.ConfirmCode = randomCode;
+				appUser.PasswordHash = userManager.PasswordHasher.HashPassword(appUser, appUserCreateVM.Password);
+
+				IdentityResult result = await userManager.CreateAsync(appUser);
+
+				if (result.Succeeded)
+				{
+					EmailSend(appUser.Email, randomCode);
+					TempData["Email"] = appUser.Email;
+					return RedirectToAction("EmailConfirm");
+				}
+				else
+				{
+					foreach (IdentityError error in result.Errors)
+					{
+						ModelState.AddModelError("Error", error.Description);
+					}
+				}
+			}
+			else ModelState.AddModelError("Error", "Something was wrong");
+			return View(appUserCreateVM);
+		}
+
+		[NonAction]
+		private void EmailSend(string email, int randomCode)
+		{
+			MailboxAddress mailboxAddressFrom = new MailboxAddress("mbf hamburger", "fatih_trkci96@hotmail.com");
+			MailboxAddress mailboxAddressTo = new MailboxAddress("User", email);
+
+			var bodyBuilder = new BodyBuilder();
+			bodyBuilder.TextBody = "Sign Up process confirm code :  " + randomCode;
+
+			MimeMessage mimeMessage = new();
+			mimeMessage.From.Add(mailboxAddressFrom);
+			mimeMessage.To.Add(mailboxAddressTo);
+
+			mimeMessage.Body = bodyBuilder.ToMessageBody();
+			mimeMessage.Subject = "mbf Hamburger";
+
+			SmtpClient client = new();
+			client.Connect("smtp.office365.com", 587, false);
+			client.Authenticate("fatih_trkci96@hotmail.com", "dakytkefxqwqevks");
+			client.Send(mimeMessage);
+			client.Disconnect(true);
+		}
+
+		public IActionResult EmailConfirm()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> EmailConfirmAsync(AppUserEmailConfirmVM appUserEmailConfirmVM)
+		{
+			AppUser appUser = await userManager.FindByEmailAsync(appUserEmailConfirmVM.Email);
+			if (appUser is not null)
+			{
+				appUser.EmailConfirmed = true;
+				IdentityResult result = await userManager.UpdateAsync(appUser);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index", "Home");
+				}
+				else
+				{
+					foreach (IdentityError error in result.Errors)
+					{
+						ModelState.AddModelError("Email confirm process is unsuccess !", error.Description);
+					}
+				}
+			}
+			else
+			{
+				ModelState.AddModelError("Error", "Something went wrong !\nA new confirm code was sent.");
+			}
+			return View(appUserEmailConfirmVM);
+		}
+
+		public IActionResult SignIn() 
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SignInAsync(AppUserSignInVM appUserSignInVM) 
+		{
+			if (ModelState.IsValid)
+			{
+				AppUser appUser = await userManager.FindByEmailAsync(appUserSignInVM.Email);
+				if (appUser is not null)
+				{
+					if (appUser.IsActive)
+					{
+						Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(appUser, appUserSignInVM.Password, false, true);
+						if (result.Succeeded)
+						{
+							if (appUser.EmailConfirmed)
+							{
+								return RedirectToAction("Index", "UserAccounts");
+							}
+							else
+							{
+								ModelState.AddModelError("Error", "Your email could not have been confirmed");
+							}
+						}
+						else ModelState.AddModelError("Error", "Email or password is incorrect !");
+					}
+				}
+				else ModelState.AddModelError("Error", "Something went wrong !");
+			}
+			return View(appUserSignInVM);
+		}
+	}
+}
